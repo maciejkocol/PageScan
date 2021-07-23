aad84e79b143f00383dc1cd925bf4a92975abb5b
Maciej Kocol <maciej.kocol@softrams.com>
2021-07-23 13:59:06 -0500

    removed cucumber ext

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
 
-code-server --install-extension alexkrechik.cucumberautocomplete
-
 DIR="/home/seluser/page-scan/"
 if [ -z "$(ls -A $DIR)" ]; then
   git clone https://github.com/maciejkocol/pagescan.git "$DIR"
